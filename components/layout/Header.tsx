a19358c6866cbf5d9fdab35c86b90249bc836567
MohammedYaseenKalvium <m.yaseensayed09@gmail.com>
2026-02-08 21:39:02 +0530

    feat: add header component and mount in layout

---
+++ components/layout/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <div>
+                    {/* {logo} */}
+                </div>
+                <div>
+                    {/* {nav links} */}
+                </div>
+            </div>
+        </header>
+    );
+}
